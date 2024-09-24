10c93a554e97a3efbcf7f5cfcdf1b86b8f2fe1c3
Sergey Shishkin <sergey@shishkin.org>
2024-09-24 20:12:56 +0200

    init astro project

---
+++ devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  # See full reference at https://devenv.sh/reference/options/
+
+  languages.javascript.enable = true;
+  languages.javascript.package = pkgs.nodejs_20;
+  languages.javascript.corepack.enable = true;
+
+  enterShell = ''
+    export PATH="$PWD/node_modules/.bin:$PATH"
+
+    corepack use pnpm
+    pnpm i
+  '';
+}
