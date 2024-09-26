9a7737d0bd67629026961ac4338baa33ec5bd86c
Sergey Shishkin <sergey@shishkin.org>
2024-09-26 22:00:14 +0200

    basic layout and content

---
+++ src/pages/index.md
@@ -0,0 +1,15 @@
+---
+layout: "~/layouts/Layout.astro"
+title: "Musikverein Bürglen"
+---
+
+Herzlich Willkommen beim Musikverein Bürglen!
+Der Musikverein Bürglen wurde im Jahre 1917 gegründet.
+Wir spielen in Harmoniebesetzung in der 2. Stärkeklasse.
+Der Verein zählt zurzeit 29 Mitglieder.
+Der Musikverein steht unter der musikalischen Leitung von Christian Simmen.
+
+Unser Probetag ist der Montag.
+Unser Ziel ist es, mit guter Blasmusik einen Beitrag zum kulturellen Leben in der Gemeinde Bürglen zu leisten.
+Jährlich führen wir zwei grössere Konzerte durch, eines im Frühjahr als Unterhaltungskonzert und eines im Dezember als Winterkonzert in der Kirche.
+Daneben umrahmen wir diverse kirchliche und weltliche Feste in der Gemeinde Bürglen.
--- uno.config.ts
+++ uno.config.ts
@@ -11,10 +11,25 @@ export default defineConfig<Theme>({
   transformers: [transformerDirectives()],
   theme: {
     colors: {
-      accent1: theme.colors?.green!,
-      accent2: theme.colors?.blue!,
+      primary: theme.colors?.cyan!,
     },
   },
-  rules: [],
-  shortcuts: {},
+  rules: [
+    [
+      /^filled-container$/,
+      (_, { theme }) => ({
+        width: "100%",
+        /**
+         * Filled container needs to have full width, but adjust horizontal padding to maintain
+         * content at max width of 6xl (72rem) minus default spacing. It also should have at least
+         * default spacing (1rem) as horizontal padding.
+         */
+        "padding-inline": `max(${theme.spacing?.DEFAULT}, calc((100% - ${theme.width?.["6xl"]}) / 2 + ${theme.spacing?.DEFAULT}))`,
+      }),
+    ],
+  ],
+  shortcuts: {
+    "wide-container": "w-full max-w-6xl mx-auto px-4",
+    heading: "font-light uppercase font-stretch-semi-condensed",
+  },
 });
