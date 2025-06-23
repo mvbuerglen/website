a493b6d5cb7598f5c3e32acd0ff943d7670b2aed
Sergey Shishkin <sergey@shishkin.org>
2025-06-23 15:26:11 +0200

    node versions

--- devenv.nix
+++ devenv.nix
@@ -4,13 +4,11 @@
   # See full reference at https://devenv.sh/reference/options/
 
   languages.javascript.enable = true;
-  languages.javascript.package = pkgs.nodejs_20;
-  languages.javascript.corepack.enable = true;
+  languages.javascript.package = pkgs.nodejs_22;
+  languages.javascript.pnpm.enable = true;
+  languages.javascript.pnpm.install.enable = true;
 
   enterShell = ''
     export PATH="$PWD/node_modules/.bin:$PATH"
-
-    corepack use pnpm@9
-    pnpm i
   '';
 }
